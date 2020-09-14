9b3b09de0c3c7d79a75343ba365d63c63e8e2efc
Jonathan Xu <jonathanxu.18@gmail.com>
2020-09-14 13:31:38 -0700

    Create workflow.py

---
+++ workflow.py
@@ -0,0 +1,18 @@
+from toil.common import Toil
+from toil.job import Job
+
+def helloWorld(job, message, memory="1G", cores=1, disk="1G"):
+    job.log(f"Hello, world! {message}")
+
+if __name__ == "__main__":
+    parser = Job.Runner.getDefaultArgumentParser()
+    options = parser.parse_args()
+    options.clean = "always"
+
+    parent_job = Job.wrapJobFn(helloWorld, "FIRST JOB")
+
+    for i in range(0, 10):
+        parent_job.addChildJobFn(helloWorld, i)
+
+    with Toil(options) as toil:
+        toil.start(parent_job)
