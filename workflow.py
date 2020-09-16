5023440bea5c0d27659c52e69292085d5963a5ad
Jonathan Xu <jonathanxu.18@gmail.com>
2020-09-16 15:43:29 -0700

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,18 +1,18 @@
 from toil.common import Toil
 from toil.job import Job
 
-def helloWorld(job, message, memory="3G", cores=2, disk="3G"):
-    job.log(f"Hello, world! {message}")
+def helloWorld(message, memory="3G", cores=2, disk="3G"):
+    return f"Hello, world! {message}"
 
 if __name__ == "__main__":
     parser = Job.Runner.getDefaultArgumentParser()
     options = parser.parse_args()
     options.clean = "always"
 
-    root_job = Job.wrapJobFn(helloWorld, "FIRST JOB")
+    root_job = Job.wrapFn(helloWorld, "FIRST JOB")
 
-    for i in range(0, 1000):
-        root_job.addChildJobFn(helloWorld, i)
+    for i in range(0, 100):
+        root_job.addChildFn(helloWorld, i)
 
     with Toil(options) as workflow:
         workflow.start(root_job)
